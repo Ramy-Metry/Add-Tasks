76aff0a9e25cb4639ae155dc2206439038674ac1
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2020-12-23 13:10:50 -0800

    reset db

--- data/seeds/tasks.js
+++ data/seeds/tasks.js
@@ -4,9 +4,10 @@ exports.seed = async function(knex) {
   await knex('tasks').del()
     .then(function () {
       
-      // return knex('tasks').insert([
-      //   {}
-        
-      // ]);
+      return knex('tasks').insert([
+        {id: 1, task: 'study nodeJS'},
+        {id: 2, task: 'Cleaning table'},
+        {id: 3, task: 'walking'}
+      ]);
     });
 };
